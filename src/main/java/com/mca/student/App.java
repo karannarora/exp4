93c22232c6e45fe760c80bc2a4c1014af763fdac
Karan Arora <arorakaran.sk@gmail.com>
2025-09-28 19:11:29 +0530

    Add files via upload

---
+++ src/main/java/com/mca/student/App.java
@@ -0,0 +1,8 @@
+package com.mca.student;
+public class App {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println("Sum is: " + (a + b));
+    }
+}
